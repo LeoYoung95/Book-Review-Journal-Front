e231a09a34cf6fa1544e673a91e17a375dd44155
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-30 02:09:34 -0800

    implement basic search bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 import { useNavigate, Link } from 'react-router-dom';
+import Searchbar from './searchbar.jsx';
 
-export default function Navbar() {
+export default function Navar() {
   const navigate = useNavigate();
 
   return (
-    <div className='bg-blue-500 mb-3 h-[60px] w-full top-0 z-50 fixed flex justify-between items-center px-4'>
+    <div className='bg-pink-500 mb-3 h-[60px] w-full top-0 z-50 fixed flex justify-between items-center px-4'>
       <button onClick={() => navigate('/')}>
         <img 
           src={logo} 
@@ -14,6 +15,9 @@ export default function Navbar() {
           className='h-[45px]'
         />
       </button>
+
+      <Searchbar />
+
       <div>
         <Link to='/signin'>Signin</Link>
       </div>
