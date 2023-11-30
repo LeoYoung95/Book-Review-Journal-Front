cc53b42b7bad9920c73834df70a1a83de83f5f53
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-30 01:24:10 -0800

    implement basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar.jsx';
+import Homepage from './components/homepage.jsx';
+import Profile from './components/profile.jsx';
+import Signup from './components/signup.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <div className='bg-blue-500'>
-        <h1>
-          Hello
-        </h1>
-      </div>
+    <div className="w-full bg-blue-500">
+      <Navbar />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />}/>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 } 
