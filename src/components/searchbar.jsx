de6c2ff6bca63d4b1fcb9220081dedf8ce9f5792
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-11-30 19:39:59 -0800

    update search bar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -18,19 +18,22 @@ export default function Searchbar() {
   return (
     <div>
       <form onSubmit={handleSearch}>
-        <input 
-          type='text'
-          value={searchInput}
-          onChange={handleInputChange}
-          placeholder='Search...'
-          className='rounded w-[450px] h-[30px] placeholder:italic placeholder:text-slate-400 border border-slate-300 '
-        />
-        <button 
-          type='submit' 
-          className='bg-yellow-500 rounded h-[30px] px-2 ml-1'
-        >
-          Search
-        </button>
+        <div className='border-b border-sky-500 py-1 '>
+          <input 
+            type='text'
+            value={searchInput}
+            onChange={handleInputChange}
+            placeholder='Search...'
+            className=' py-2 px-2 
+            rounded w-[300px] h-[30px] placeholder:italic placeholder:text-slate-400'
+          />
+          <button 
+            type='submit' 
+            className='bg-sky-200 rounded h-[30px] px-2 ml-1'
+          >
+            Search
+          </button>
+        </div>
       </form>
     </div>
   )
