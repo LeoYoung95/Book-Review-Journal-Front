20545edc1626efe93fd9845a3eb99e2bc449e805
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-12-01 10:32:04 -0800

    add role to signup

--- src/components/auth/signup.jsx
+++ src/components/auth/signup.jsx
@@ -57,10 +57,27 @@ export default function Signup() {
             </div>
           </div>
 
+          <div class="md:flex md:items-center mb-6">
+            <div class="md:w-1/3">
+              <label className='block md:text-right mb-1 md:mb-0 pr-4'>
+                Role
+              </label>
+            </div>
+            <div class="md:w-2/3">
+              <select className="border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-sky-200">
+                <option>Reader</option>
+                <option>Write</option>
+              </select>
+            </div>
+          </div>
+
           <div class="md:flex md:items-center">
             <div class="md:w-1/3"></div>
             <div class="md:w-2/3">
-              <button className="bg-sky-500 hover:bg-sky-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" onClick={signup}> Register and sign in </button>
+              <button className="bg-sky-500 hover:bg-sky-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" 
+                onClick={signup}> 
+                Register and sign in 
+              </button>
             </div>
           </div>
         </form>
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import personal_info from './personal_info';
-import liked_reviews_reader from './liked_reviews_reader';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Profile() {
+  const {pathname} = useLocation();
   return (
     <div className='w-full ms-2'>
       <h1>Profile</h1>
-      <ul className="flex" 
-        role="tablist"
-        data-te-nav-ref>
-        <li className="mr-3" role="presentation">
-          <Link class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" 
-            to={personal_info}>
-            Personal Info
-          </Link>
-        </li>
-        <li className="mr-3" role="presentation">
-          <Link class="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" 
-            to={liked_reviews_reader}>
-            Liked Reviews
-          </Link>
-        </li>
-      </ul>
+      {pathname}
+      <nav className="nav nav-pills mt-2">
+        
+        <Link className={`nav-link ${pathname.includes("a3") ? "active" : ""}`} 
+              to="/profile/personal_info">A3</Link>
+        <Link className={`nav-link ${pathname.includes("a4") ? "active" : ""}`} 
+              to="/Labs/a4">A4</Link>
+
+      </nav>
+      
 
     </div>
   )
