309af6a0190558c3a52112b0d7201a832de3f4d4
yangxu95 <yang.xu6@northeastern.edu>
2023-12-07 14:15:31 -0800

    Update reader components

--- src/components/homepages/ReaderHome.jsx
+++ src/components/homepages/ReaderHome.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import ReaderLikedReviews from '../reader/readerLikedReview';
 
 const ReaderHome = () => {
     const currentUser = useSelector(state => state.currentUser); // Assuming this is how you access the current user
 
+    // Check if the user is an Admin
+    if (currentUser && currentUser.role === 'Reader') {
+        return <ReaderLikedReviews />;
+    }
 
-    return <div>Reader Home</div>;
 };
 
 export default ReaderHome;
